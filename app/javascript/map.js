16c6779a9d85329e637eb3236453c6596ac5af58
Amos <alphatrl1997@gmail.com>
2020-11-13 11:06:06 +0800

    Add google map directions
    - Move everything into a single vue instance

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,56 +1,5 @@
-const vm = new Vue({
-    el: "#info",
-    data: {
-        name: "",
-        address: "",
-        foursq_url: "",
-        price: {},
-        desc: "",
-        categories: [],
-        photo: "",
-    },
-    computed: {
-        distance: function () {
-            const queryString = window.location.search;
-            const params = new URLSearchParams(queryString);
-            const distance = params.get("distance");
-            return distance;
-        },
-    },
-    mounted: function () {
-        const queryString = window.location.search;
-        const params = new URLSearchParams(queryString);
-        const id = params.get("id");
-        const url =
-            "https://api.foursquare.com/v2/venues/" +
-            id +
-            `?client_id=${foursquare.client_id}&client_secret=${foursquare.client_secret}&v=20201020`;
-
-        axios
-            .get(url)
-            .then((response) => {
-                const venue = response.data.response.venue;
-                this.name = venue.name;
-                this.address = venue.location.address;
-                this.foursq_url = venue.canonicalUrl;
-                if (venue.hasOwnProperty("price")) {
-                    this.price = venue.price;
-                }
-                if (Object.keys(venue).includes("description")) {
-                    this.desc = venue.description;
-                }
-                this.categories = venue.categories;
-                this.photo =
-                    venue.bestPhoto.prefix +
-                    "cap300" +
-                    venue.bestPhoto.suffix;
-            })
-            .catch((error) => alert(error));
-    },
-});
-
-const nav = new Vue({
-    el: '#nav',
+const main = new Vue({
+    el: '#main',
     data: {
         // links for navbar
         links: {
@@ -62,10 +11,31 @@ const nav = new Vue({
             'login': './sites/login.php',
         },
 
+        // map variables
+        name: "",
+        address: "",
+        foursq_url: "",
+        price: {},
+        desc: "",
+        categories: [],
+        photo: "",
+        currentPos: "",
+        venuePos: "",
+
+
         is_user_login: false,
     },
-    mounted: function () {
-        this.getUserInfo();
+    mounted: async function () {
+        await this.fetchData();
+        await this.getUserInfo();
+    },
+    computed: {
+        distance: function () {
+            const queryString = window.location.search;
+            const params = new URLSearchParams(queryString);
+            const distance = params.get("distance");
+            return distance;
+        },
     },
     methods: {
         getUserInfo: async function() {
@@ -74,6 +44,43 @@ const nav = new Vue({
                     var user = res.data.user
                     this.is_user_login = true;
                 })
+        },
+        fetchData: async function() {
+            const queryString = window.location.search;
+            const params = new URLSearchParams(queryString);
+            const id = params.get("id");
+            const url =
+                "https://api.foursquare.com/v2/venues/" +
+                id +
+                `?client_id=${foursquare.client_id}&client_secret=${foursquare.client_secret}&v=20201020`;
+            this.venuePos = params.get('venuePos');
+            this.currentPos = params.get('currentPos');
+            // this.venuePos = {lat: venuePosCombine[0], lng: venuePosCombine[1]};
+            // this.currentPos = {lat: currentPosCombine[0], lng: currentPosCombine[1]};
+            
+            axios
+                .get(url)
+                .then((response) => {
+                    const venue = response.data.response.venue;
+                    this.name = venue.name;
+                    this.address = venue.location.address;
+                    this.foursq_url = venue.canonicalUrl;
+                    if (venue.hasOwnProperty("price")) {
+                        this.price = venue.price;
+                    }
+                    if (Object.keys(venue).includes("description")) {
+                        this.desc = venue.description;
+                    }
+                    this.categories = venue.categories;
+                    this.photo =
+                        venue.bestPhoto.prefix +
+                        "cap300" +
+                        venue.bestPhoto.suffix;
+                })
+                .catch((error) => alert(error));
+        },
+        openMapApp: function() {
+            window.location.href = `https://www.google.com/maps/dir/?api=1&origin=${this.currentPos}&destination=${this.venuePos}`
         },
     },
 })
